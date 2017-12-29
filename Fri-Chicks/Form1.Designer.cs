412ed4c67253e1d8b8b98d3f3f37744645547a43
HASSAN ARSHAD <hassanarshad115@gmail.com>
2017-12-29 12:39:10 -0800

    Fri-Chicks Project

--- Fri-Chicks/Form1.Designer.cs
+++ Fri-Chicks/Form1.Designer.cs
@@ -117,7 +117,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(503, 12);
+            this.label1.Location = new System.Drawing.Point(502, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(299, 76);
             this.label1.TabIndex = 1;
